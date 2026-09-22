e8e6fad1c9909291f14a44354da42272d6f2c2fa
Andrew Echeverria <andrewe589@gmail.com>
2026-09-22 18:56:39 -0400

    Add multiple SELECT queries for data retrieval

--- SQL/checks.sql
+++ SQL/checks.sql
@@ -1,1 +1,29 @@
+SELECT *
+FROM employees;
 
+SELECT COUNT(*) AS dependent_count
+FROM dependents;
+
+SELECT COUNT(*) AS plan_count
+FROM benefit_plans;
+
+SELECT COUNT(*) AS enrollment_count
+FROM enrollments;
+
+SELECT *
+FROM employees
+LIMIT 100;
+
+SELECT *
+FROM dependents
+LIMIT 100;
+
+SELECT *
+FROM enrollments
+LIMIT 100;
+
+SELECT *
+FROM benefit_plans;
+
+SELECT *
+FROM data_quality_errors;
